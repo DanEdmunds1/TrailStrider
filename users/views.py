bd00b7ab7f48133b9693d2f82fe0d9e7664acd4f
Dan <dan.edmunds7@gmail.com>
2024-01-22 10:42:16 +0000

    h

--- users/views.py
+++ users/views.py
@@ -1,9 +1,6 @@
 from rest_framework.generics import CreateAPIView
 from .serializers.common import RegistrationSerializer, UserSerializer
 from django.contrib.auth import get_user_model
-from lib.views import OwnerListCreateView
-from .serializers.common import UserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 User = get_user_model()
 
