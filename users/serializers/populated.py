b7c56f932bd016eb26d8ffef133c089bf4ae7fad
Dan <dan.edmunds7@gmail.com>
2024-01-22 11:04:16 +0000

    h

--- users/serializers/populated.py
+++ users/serializers/populated.py
@@ -1,7 +1,5 @@
 from .common import RegistrationSerializer
 from hikers.serializers.common import HikerSerializer
-from reviews.serializers.common import ReviewSerializer
 
 class PopulatedUserSerializer(RegistrationSerializer):
     hikers = HikerSerializer(many=True)
-    reviews = ReviewSerializer(many=True)
