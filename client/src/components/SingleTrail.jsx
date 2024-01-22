ffa682c954f299b52e5a78a86db1b0778991ea49
Dan <dan.edmunds7@gmail.com>
2024-01-22 10:36:48 +0000

    message

--- client/src/components/SingleTrail.jsx
+++ client/src/components/SingleTrail.jsx
@@ -1,5 +1,4 @@
 import { useNavigate, useLoaderData, Link } from "react-router-dom"
-import { useNavigate, useLoaderData, Link } from "react-router-dom"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
